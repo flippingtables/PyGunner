99513fa1febad206f63d9f1e1269721ce3309a1d
Joannes I Sandagerdi <joannes@sandagerdi.com>
2016-01-22 23:05:00 +0100

    Fix for install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 chmod +x PyGunner.py
 
 # Make a symlink to /usr/local/bin/, so that we can call the script from anywhere
-ln -s PyGunner.py /usr/local/bin/pygunner
+ln -s `pwd`/PyGunner.py /usr/local/bin/pygunner
