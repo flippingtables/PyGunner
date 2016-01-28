727294ef65e5775e51545edf5d676bbe13548303
Jóannes í Sandagerði <fragflop@yahoo.dk>
2016-01-28 11:39:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # Make PyGunner.py executable
 chmod +x PyGunner.py
 
-# Make a symlink to /usr/local/bin/, so that we can call the script from anywhere
-ln -s `pwd`/PyGunner.py /usr/local/bin/pygunner
+# Copy the binary to /usr/local/bin/pygunner, and store the config file in /etc/pygunner/config.json
+sudo cp PyGunner.py /usr/local/bin/pygunner
+sudo mkdir /etc/pygunner
+sudo cp config.json /etc/pygunner/config.json
